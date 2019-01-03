3408574f812d4432bc14c749eb853d21a0202956
Maciej Kusper <maciek.kusper@gmail.com>
2019-01-03 14:34:46 +0100

    Add customer to datebase work fine

--- src/main/java/skirental/controllers/CustomerController.java
+++ src/main/java/skirental/controllers/CustomerController.java
@@ -1,8 +1,34 @@
 package skirental.controllers;
 
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import org.omg.CORBA.portable.ApplicationException;
+import skirental.models.CustomerModel;
+
 
 public class CustomerController {
-    public void addCustomer() {
+
+    @FXML
+    private Button newCustomerButton;
+
+    @FXML
+    private TextField nameText;
+    @FXML
+    private TextField surnameText;
+    @FXML
+    private TextField addresText;
+    @FXML
+    private TextField cardText;
+
+    private CustomerModel customerModel;
+    @FXML
+    public void initialize(){
+        this.customerModel = new CustomerModel();
+    }
+
+    public void addCustomer() throws ApplicationException {
+
+        customerModel.saveCustomerToDB(nameText.getText(),surnameText.getText(),addresText.getText(),cardText.getText());
     }
 }
